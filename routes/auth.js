e36da20c651c9a85598ad41fd3c60bbc9b112167
Michael Yeager <m.yeager001@gmail.com>
2016-01-11 11:52:49 -0700

    add local strategy

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var session = require('express-session');
 var passport = require('passport');
+var bcrypt = require('bcrypt');
+var knex = require('../db/knex');
+LocalStrategy = require('passport-local');
 GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 require('dotenv').load();
 
@@ -17,6 +20,26 @@ router.use(session({ secret: process.env.SESSION_SECRET }));
 router.use(passport.initialize());
 router.use(passport.session());
 
+//local strategy
+passport.use(new LocalStrategy(function(username, password, done){
+  console.log(password);
+  knex('users').select().where('username', username).first()
+  .then(function(user){
+    console.log(user);
+    if(user && bcrypt.compareSync(password, user.hash)){
+      return done(null, user);
+    }else{
+      console.log('here')
+      return done(null, false, {message: 'invalid username or password'});
+    }
+  })
+  .catch(function(error){
+    console.log(error);
+    return done(error);
+  });
+}));
+
+//google strategy
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CONSUMER_KEY,
     clientSecret: process.env.GOOGLE_CONSUMER_SECRET,
@@ -26,17 +49,47 @@ passport.use(new GoogleStrategy({
     return done(null, profile);
   })
 );
+//localstratgey routes
+router.post('/auth/login',
+ passport.authenticate('local', {failureRedirect: '/login'}),
+ function(req, res){
+   res.redirect('/search');
+});
 
+router.post('/auth/signup', function(req, res){
+  knex('users').select().where('username', req.body.username).first().then(function(user){
+    if(!user){
+      knex('users').insert({username: req.body.username, hash: bcrypt.hashSync(req.body.password, 10)})
+      .then(function(){
+        res.redirect('/');
+      }).catch(function(error){
+        console.error('could not enter user');
+        return done(error);
+      });
+    }else{
+      console.error('username already exits');
+      res.redirect('/');
+    }
+    }).catch(function(error){
+      console.error('could not serach databse');
+      return done(error);
+    });
+});
+
+
+
+//routes for google with callback
 router.get('/auth/google',
   passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
 
 router.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
+  passport.authenticate('google', { failureRedirect: '/' }),
   function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/users');
+    res.redirect('/search');
   });
 
+//all strategies logout
 router.get('/logout', function(req, res){
   req.logout();
   res.redirect('/login');
