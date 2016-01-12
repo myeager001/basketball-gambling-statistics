e70b90ef300bbef233c3a14ec141ec37196f6979
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2016-01-12 14:28:31 -0700

    added request

--- api/nbastats.js
+++ api/nbastats.js
@@ -1,4 +1,6 @@
-
+var express = require('express');
+var router = express.Router();
+var request = require('request');
 // API
 // https://probasketballapi.com/docs/v2
 
@@ -32,8 +34,7 @@ var options = {
 
 
 
-var $url_playerStats = 'https://probasketballapi.com/stats/players';
-var $url_players = 'https://probasketballapi.com/players';
+var $url_playerStats = 'http://api.probasketballapi.com/player';
 var $url_team = 'https://cors-anywhere.herokuapp.com/http://api.probasketballapi.com/team';
 var $url_game = 'https://cors-anywhere.herokuapp.com/http://api.probasketballapi.com/game';
 var $url_shotCharts = 'https://cors-anywhere.herokuapp.com/http://api.probasketballapi.com/shots';
@@ -41,30 +42,52 @@ var $url_playerBox = 'https://cors-anywhere.herokuapp.com/http://api.probasketba
 
 var $api_key = process.env.API_KEY;
 
-//player search by first name (example test)
-function searchPlayer(){
-  var first = document.getElementById('searchName').value;
-  $.post($url_players + "?api_key=" + $api_key + '&first_name=' + first, function(player) {
-    console.log(player);
-  })
+
+
+function callback(err, res, body) {
+  if (!err && res.statusCode == 200) {
+    console.log(body);
+  } else {
+    console.log(res.statusCode);
+    console.log(err);
+  }
 }
 
-var playerSearch = document.getElementById('playerSearchButton');
-playerSearch.addEventListener('click', searchPlayer);
 
-
-//team search by team name
- function searchTeam(){
-   var team = document.getElementById('recipeSearch2').value;
-   $.post($url_team + "?api_key=" + $api_key + '&team_name=' + team, function(team) {
-     console.log(team);
-   })
- }
-
- //game search
-  function searchGame(){
-    var date = document.getElementById('recipeSearch3').value;
-    $.post($url_game + "?api_key=" + $api_key + '&date=' + date, function(match) {
-      console.log(match);
-    })
+router.post('/', function(req, res) {
+  var first = req.body.searchFirst;
+  var last = req.body.searchLast;
+  var options = {
+    url: urlDone + "&first_name=" + first + "&last_name=" + last,
+    json: true
   }
+  request.post(options, callback)
+})
+//
+// //player search by first name (example test)
+// function searchPlayer(){
+//   var first = document.getElementById('searchName').value;
+//   $.post($url_players + "?api_key=" + $api_key + '&first_name=' + first, function(player) {
+//     console.log(player);
+//   })
+// }
+//
+// var playerSearch = document.getElementById('playerSearchButton');
+// playerSearch.addEventListener('click', searchPlayer);
+//
+//
+// //team search by team name
+//  function searchTeam(){
+//    var team = document.getElementById('recipeSearch2').value;
+//    $.post($url_team + "?api_key=" + $api_key + '&team_name=' + team, function(team) {
+//      console.log(team);
+//    })
+//  }
+//
+//  //game search
+//   function searchGame(){
+//     var date = document.getElementById('recipeSearch3').value;
+//     $.post($url_game + "?api_key=" + $api_key + '&date=' + date, function(match) {
+//       console.log(match);
+//     })
+//   }
