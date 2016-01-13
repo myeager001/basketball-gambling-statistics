599df4f6333f7ec0dff5c6b19dcd31cc62f74a30
Justin Lawson <jaglawson3@gmail.com>
2016-01-13 11:24:52 -0700

    added classes to images on landing.jade, began editing styling on landing.css

--- src/javascript/test.js
+++ src/javascript/test.js
@@ -1,20 +1,8 @@
-// $(document).ready(function(){
-//   $('.action').on('click', function(e){
-//     console.log('here1')
-//     var tag = '.'+$(this).text();
-//     $('.active').toggleClass('active');
-//     $(tag).toggleClass('active');
-//   })
-//
-//   $('.user-icon').on('click', function(e){
-//     var selector = $('.dropDownMenu1');
-//     var hasClass = selector.hasClass('active');
-//     console.log(hasClass)
-//     if(hasClass){
-//       console.log('here')
-//       selector.removeClass('active');
-//     }else{
-//       selector.addClass('active');
-//     }
-//   })
-// })
+$(document).ready(function(){
+  $('.action').on('click', function(e){
+    console.log('here1')
+    var tag = '.'+$(this).text();
+    $('.active').toggleClass('active');
+    $(tag).toggleClass('active');
+  })
+})
