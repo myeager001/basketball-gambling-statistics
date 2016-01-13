2ff7454626b8393004a83dcc51b6f36cea0b645c
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2016-01-13 15:30:21 -0700

    :gun: shot charts algorithm started

--- routes/search.js
+++ routes/search.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var request = require('request');
 var efficiency = require('../algorithms/compareEff');
 var team4factor = require('../algorithms/team4factor');
+var shotCharts = require('../algorithms/shotCharts');
 
 
 /* GET users listing. */
@@ -10,20 +11,16 @@ router.get('/', function(req, res, next) {
   res.render('search', {user: req.user});
 });
 
-var apiKey = "?api_key=" + process.env.API_KEY;
-var url_team = 'http://api.probasketballapi.com/team' + apiKey;
-var url_teamAdv = 'http://api.probasketballapi.com/advanced/team' + apiKey;
-var url_team4factor = 'http://api.probasketballapi.com/four_factor/team' + apiKey;
-
 router.post('/', function(req,res){
   var team1 = req.body.firstTeam;
   var team2 = req.body.secondTeam;
 
 
-  Promise.resolve(efficiency(team1, team2)).then(function(results){
-    carray(results);
-  });
-  team4factor(team1, team2);
+  // Promise.resolve(efficiency(team1, team2)).then(function(results){
+  //   carray(results);
+  // });
+  // team4factor(team1, team2);
+  shotCharts(team1, team2);
 
 
 
