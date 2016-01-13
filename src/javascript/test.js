d3f35af40978b65ec7ccdf92534fa4cafc517586
Addison <addisonbennett@gmail.com>
2016-01-13 13:20:17 -0700

    jhfdsjhksd

--- src/javascript/test.js
+++ src/javascript/test.js
@@ -5,8 +5,4 @@ $(document).ready(function(){
     $('.active').toggleClass('active');
     $(tag).toggleClass('active');
   })
-<<<<<<< HEAD
-
-=======
->>>>>>> 05232d6587d1393e17e7494bb394f487c899e998
 })
