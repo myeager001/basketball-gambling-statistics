5985401fc0a1cb44d8f3f6e0cc8aed5c09a6e1dc
Michael Yeager <m.yeager001@gmail.com>
2016-01-14 20:40:16 -0700

    preferences

--- seeds/stats.js
+++ seeds/stats.js
@@ -7,13 +7,13 @@ exports.seed = function(knex, Promise) {
     // Inserts seed entries
     knex('stats').insert({id: 1, name: 'boxScore', catagory: 'catagory1'}),
     knex('stats').insert({id: 2, name: 'compareEff', catagory: 'catagory1'}),
-    knex('stats').insert({id: 3, name: 'pointsOverTime', catagory: 'catagory1'}),
+    knex('stats').insert({id: 3, name: 'missHit', catagory: 'catagory1'}),
     knex('stats').insert({id: 4, name: 'shotCharts', catagory: 'catagory1'}),
     knex('stats').insert({id: 5, name: 'sportsVu', catagory: 'catagory2'}),
     knex('stats').insert({id: 6, name: 'teamFourFactor', catagory: 'catagory2'}),
     knex('stats').insert({id: 7, name: 'teamMisc', catagory: 'catagory2'}),
-    knex('stats').insert({id: 8, name: 'stat8', catagory: 'catagory2'}),
-    knex('stats').insert({id: 9, name: 'stat9', catagory: 'catagory3'}),
-    knex('stats').insert({id: 10, name: 'stat10', catagory: 'catagory3'}),
+    knex('stats').insert({id: 8, name: 'offEffOverTime', catagory: 'catagory2'}),
+    knex('stats').insert({id: 9, name: 'defEffOverTime', catagory: 'catagory3'}),
+    knex('stats').insert({id: 10, name: 'shooting', catagory: 'catagory3'}),
   ]);
 };
